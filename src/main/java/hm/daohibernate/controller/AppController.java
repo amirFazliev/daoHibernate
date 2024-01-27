5c49390b1acc9db90c3bac7d0f28ab603fbf288c
Amir Fazliev <fazliev.amir@yandex.ru>
2024-01-27 22:19:33 +0300

    commit upgrade code - security

--- src/main/java/hm/daohibernate/controller/AppController.java
+++ src/main/java/hm/daohibernate/controller/AppController.java
@@ -8,7 +8,6 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 
-import java.sql.SQLException;
 import java.util.List;
 import java.util.Optional;
 
---
+++ src/main/java/hm/daohibernate/security/SecurityConfig.java
@@ -0,0 +1,38 @@
+package hm.daohibernate.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.UserDetailsService;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private UserDetailsService userDetailsService;
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .antMatchers("/persons/by-city").permitAll() // доступ без авторизации
+                .antMatchers("/persons/**").authenticated() // доступ только после авторизации
+                .anyRequest().authenticated()
+                .and()
+                .formLogin()
+                .loginPage("/login")
+                .defaultSuccessUrl("/dashboard") // страница после успешного входа
+                .permitAll()
+                .and()
+                .logout()
+                .permitAll();
+    }
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userDetailsService);
+    }
+}
