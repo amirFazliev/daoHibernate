10c84ff5165b4dc2f7dfede649896d11a2a29370
Amir Fazliev <fazliev.amir@yandex.ru>
2024-01-27 22:29:25 +0300

    commit upgrade code - security2

--- src/main/java/hm/daohibernate/security/SecurityConfig.java
+++ src/main/java/hm/daohibernate/security/SecurityConfig.java
@@ -1,11 +1,16 @@
 package hm.daohibernate.security;
 
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration
 @EnableWebSecurity
@@ -16,23 +21,20 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http
-                .authorizeRequests()
-                .antMatchers("/persons/by-city").permitAll() // доступ без авторизации
-                .antMatchers("/persons/**").authenticated() // доступ только после авторизации
-                .anyRequest().authenticated()
+        http.authorizeRequests()
+                .antMatchers("/persons/create").permitAll() // разрешаем доступ без авторизации
+                .anyRequest().authenticated() // требуем авторизацию для всех остальных endpoint
                 .and()
-                .formLogin()
-                .loginPage("/login")
-                .defaultSuccessUrl("/dashboard") // страница после успешного входа
-                .permitAll()
-                .and()
-                .logout()
-                .permitAll();
+                .formLogin(); // используем стандартную форму логина от Spring
     }
 
-    @Autowired
-    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.userDetailsService(userDetailsService);
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
