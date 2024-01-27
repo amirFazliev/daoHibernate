a7e59c6c8125d1984be1ccee05bfd4a3ec5a3ee6
Amir Fazliev <fazliev.amir@yandex.ru>
2024-01-27 22:34:14 +0300

    commit upgrade code - security3

--- src/main/java/hm/daohibernate/DaoHibernateApplication.java
+++ src/main/java/hm/daohibernate/DaoHibernateApplication.java
@@ -2,8 +2,10 @@ package hm.daohibernate;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
 @SpringBootApplication
+@EnableWebSecurity
 public class DaoHibernateApplication {
 
 
